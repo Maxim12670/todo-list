dcc18c83891c3b5cc5fdf1ceb6dccfc7a023ddef
Максим <akaikinmax@mail.ru>
2024-05-29 00:51:48 +0700

    fix / убрал лишние типы в ITaskList

--- my-app/src/components/taskList/TaskList.tsx
+++ my-app/src/components/taskList/TaskList.tsx
@@ -1,4 +1,6 @@
+import { FC } from 'react';
 import { ContentContainer, CardTask } from '../index';
+import { ITaskList } from '../../lib/types';
 import styled from 'styled-components';
 
 const List = styled.div`
@@ -7,16 +9,17 @@ const List = styled.div`
   gap: 1.6rem;
   margin-top: 1.5rem`;
 
-const TaskList = () => {
+const TaskList: FC<ITaskList> = ({ tasks}) => {
 
   return (
     <ContentContainer>
       <List>
-        <CardTask />
-        <CardTask />
-        <CardTask />
+        {tasks.map((task) => (
+          <CardTask
+            key={task.id}
+            task={task}/>
+        ))}
       </List>
-
     </ContentContainer >
   );
 }
