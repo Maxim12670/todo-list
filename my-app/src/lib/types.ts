839433b41522db835b6c149ce2326b89fffe7ca5
Максим <akaikinmax@mail.ru>
2024-05-29 00:50:39 +0700

    feat / добавлены интерфейсы и типы

--- my-app/src/lib/types.ts
+++ my-app/src/lib/types.ts
@@ -1,7 +1,20 @@
-export interface Task {
-  id: number,
-  title: string,
-  description: string,
-  status: boolean,
-  favorite: boolean
+export interface ITaskAttributes {
+  title: string;
+  description: string;
+  status: string
 }
+
+export interface ITask {
+  id: number;
+  attributes: ITaskAttributes
+  favorite?: string;
+}
+
+export interface ITaskList {
+  tasks: ITask[];
+  onChangeFavorite: (id: number) => void;
+  onChangeStatus: (id: number) => void;
+  onDeleteTask: (id: number) => void;
+}
+
+export type TFilterTask = 'all' | 'active' | 'completed' | 'favorite' | 'notfavorite';
